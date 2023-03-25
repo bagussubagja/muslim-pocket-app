80b0d4b21ee58e3bf07fc1d2136b29a6824bbff1
bagussubagja <bagussubagja17@gmail.com>
2023-03-25 20:59:46 +0700

    MP-29 : Add API KEY note

--- lib/app/utils/constants/constant_id_key.dart
+++ lib/app/utils/constants/constant_id_key.dart
@@ -1,4 +1,5 @@
 class ConstantIdKey {
+  // Add Youtube API Key here
   String youtubeAPIKey = "";
   String shahihFiqhChannelId = "UClCl3I9DfH4HUty22wPF9eg";
   String defaultSubdistrict = 'jakarta';
